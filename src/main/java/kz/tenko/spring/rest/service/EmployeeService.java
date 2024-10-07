c84d3a7f590cd54f08c4b2c03c0d42a1ef8ae292
Tenko <tenko_92@mail.ru>
2024-10-07 15:00:18 +0500

    add files

---
+++ src/main/java/kz/tenko/spring/rest/service/EmployeeService.java
@@ -0,0 +1,16 @@
+package kz.tenko.spring.rest.service;
+
+
+import kz.tenko.spring.rest.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+    public List<Employee> getAllEmployees();
+
+    public void saveEmployee(Employee employee);
+
+    public Employee getEmployee(int id);
+
+    public void deleteEmployee(int id);
+}
---
+++ src/main/java/kz/tenko/spring/rest/service/EmployeeServiceImpl.java
@@ -0,0 +1,63 @@
+package kz.tenko.spring.rest.service;
+
+import kz.tenko.spring.rest.dao.EmployeeDAO;
+import kz.tenko.spring.rest.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+
+    @Autowired
+    private EmployeeDAO employeeDAO;
+
+    @Override
+    @Transactional
+    public List<Employee> getAllEmployees() {
+        return employeeDAO.getAllEmployees();
+    }
+
+    @Override
+    @Transactional
+    public void saveEmployee(Employee employee) {
+        employeeDAO.saveEmployee(employee);
+    }
+
+    @Override
+    @Transactional
+    public Employee getEmployee(int id) {
+        return employeeDAO.getEmployee(id);
+    }
+
+    @Override
+    @Transactional
+    public void deleteEmployee(int id) {
+        employeeDAO.deleteEmployee(id);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
